b6f2fad40f54af1299e9001f4a4ff0d18deaa61c
mariusz <mariusz.pohorylo@gmail.com>
2018-09-02 09:14:46 +0200

    recipe details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('Fish with sparages', 'Example description', 'https://www.eatwell101.com/wp-content/uploads/2017/03/' +
+      'Garlic-Butter-Chicken-asparagus-recipes.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
