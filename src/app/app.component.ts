46b2b7eefe062e31af88f9d733f48b78486d7cfc
mariusz <mariusz.pohorylo@gmail.com>
2018-09-03 20:21:22 +0200

    add navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Recipe book';
+  feature = 'recipe';
+  onFeatureSelected(feature: string) {
+  this.feature = feature;
+  }
 }
